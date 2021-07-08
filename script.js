7548f1e9b28f6e803eb0cc20d71f5c4b0c4b2cb2
Brian Kim <brian.kyounghoon.kim@gmail.com>
2021-07-08 09:33:30 -0400

    loading function and complete function added

--- script.js
+++ script.js
@@ -4,12 +4,25 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+// show loading
+const loading = () => {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+};
+
+const complete = () => {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+};
 
 // Get Quotes from API
 
 let apiQuotes = [];
 
 const newQuote = () => {
+  loading();
   //  pick random quotes
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -25,11 +38,13 @@ const newQuote = () => {
   } else {
     quoteText.classList.remove("long-quote");
   }
-
+  // set quote, hide loader
   quoteText.textContent = quote.text;
+  complete();
 };
 
 const getQuotes = async () => {
+  loading();
   const apiURL = `https://type.fit/api/quotes`;
   try {
     const res = await fetch(apiURL);
@@ -49,7 +64,6 @@ const tweet = () => {
 
 // event listener
 newQuoteBtn.addEventListener("click", newQuote);
-
 twitterBtn.addEventListener("click", tweet);
 
 // on load
